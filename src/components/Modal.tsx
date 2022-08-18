07ce5bc7656504b3789f1151492abfe80f1d4306
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-08-18 19:09:05 +0200

    first version

---
+++ src/components/Modal.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+  isCorrect: boolean;
+  turn: number;
+  solution: string;
+};
+export const Modal = ({ isCorrect, turn, solution }: Props) => {
+  return (
+    <div className="modal">
+      {isCorrect && (
+        <div>
+          <h1>You Win!</h1>
+          <p className="solution">{solution}</p>
+          <p>You found the solution in {turn} guesses :)</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Nevermind</h1>
+          <p className="solution">{solution}</p>
+          <p>Better luck next time :)</p>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,36 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useWordle } from "../hooks/useWordle";
 import { Keypad } from "./Keypad";
+import { Modal } from "./Modal";
 import { WordleGrid } from "./WordleGrid";
 
 type Props = {
   solution: string;
 };
 const Wordle = ({ solution }: Props) => {
+  const [showModal, setShowModal] = useState(false);
   const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys } =
     useWordle(solution);
 
   useEffect(() => {
+    // console.log("adding listener");
     window.addEventListener("keyup", handleKeyUp);
 
-    return () => window.removeEventListener("keyup", handleKeyUp);
-  }, [handleKeyUp]);
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      return () => window.removeEventListener("keyup", handleKeyUp);
+    }
 
-  useEffect(() => {
-    console.log({ guesses, isCorrect, turn });
-  }, [guesses, isCorrect, turn]);
+    if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      console.log("you fucked it up");
+      window.removeEventListener("keyup", handleKeyUp);
+    }
+
+    return () => {
+      // console.log("removing listener");
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyUp, isCorrect]);
 
   return (
     <>
-      <div>Solution - {solution}</div>
-      <div>Current guess - {currentGuess}</div>
-      <div className="">guesses - {JSON.stringify(guesses)}</div>
       <WordleGrid
         currentGuess={currentGuess}
         guesses={guesses}
         turn={turn}
       ></WordleGrid>
       <Keypad usedKeys={usedKeys}></Keypad>
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </>
   );
 };
